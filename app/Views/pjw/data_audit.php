47de35e466b61f73c70c63c4f764b6184968c797
ameliatantys <ameliatanty@1798@upi.edu>
2020-10-05 14:07:18 +0700

    menambah laman tambah data audit

--- app/Views/pjw/data_audit.php
+++ app/Views/pjw/data_audit.php
@@ -3,11 +3,6 @@
 <!-- Untuk mulai mengisi konten yang akan ditampilkan -->
 <?= $this->section('content'); ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-md-3 ml-auto">Selamat Datang, Tanti Amelia Sopya</div>
-  </div>
-</div>
 
 <br>
 <br>
@@ -21,22 +16,24 @@
   </div>
 </div>
 
-<br>
+<!-- <br>
 <div class="container">
   <button type="button" class="btn btn-danger">Tambah Data</button>
 </div>
-<br>
+<br> -->
 
+
+<br>
 <!-- Untuk filter -->
 <div class="container">
-  <div class="row">
-    <div class="col-sm-3">
-      <h6>Jenis:</h6>
+  <div class="form-inline">
+    <div class="col-sm-2">
+      <label class="d-inline-block">Jenis: </label>
       <div class="btn-group">
         <button type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Pilih Jenis
         </button>
-        <div class="dropdown-menu dropdown-menu-right">
+        <div class="dropdown-menu dropdown-menu-right dropdown-inline">
           <button class="dropdown-item" type="button">ISO</button>
           <button class="dropdown-item" type="button">Another action</button>
           <button class="dropdown-item" type="button">Something else here</button>
@@ -44,8 +41,8 @@
       </div>
     </div>
 
-    <div class="col-sm-3">
-      <h6>Area:</h6>
+    <div class="col-sm-2">
+      <label class="d-inline-block">Area : </label>
       <div class="btn-group">
         <button type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Pilih Area
@@ -58,31 +55,24 @@
       </div>
     </div>
 
-    <div class="col-sm-3">
-      <h6>Status:</h6>
-      <div class="dropdown">
-        <div class="btn-group col-sm-3">
-          <button type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Pilih Status
-          </button>
-          <div class="dropdown-menu dropdown-menu-right">
-            <button class="dropdown-item" type="button">All Status</button>
-            <button class="dropdown-item" type="button">OPEN</button>
-            <button class="dropdown-item" type="button">CLOSE</button>
-          </div>
+    <div class="col-sm-2">
+      <label class="d-inline-block">Status : </label>
+      <div class="btn-group">
+        <button type="button" class="btn btn-outline-secondary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Pilih Area
+        </button>
+        <div class="dropdown-menu dropdown-menu-right">
+          <button class="dropdown-item" type="button">Action</button>
+          <button class="dropdown-item" type="button">Another action</button>
+          <button class="dropdown-item" type="button">Something else here</button>
         </div>
       </div>
     </div>
-    <div class="col-sm-3">
-      <nav class="navbar navbar-light bg-light">
-        <form class="form-inline">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </nav>
-    </div>
   </div>
+
 </div>
+</div>
+<br>
 
 <!-- Tabel Semua Judul -->
 <div class="container">
@@ -146,10 +136,92 @@
             <td class="text-center">OPEN</td>
             <td class="text-center">1</td>
           </tr>
+          <tr>
+            <td class="text-center">1</td>
+            <td class="text-center">Internal ISMS</td>
+            <td class="text-center">ISO200781:2002 TPK Sriwidjaya</td>
+            <td class="text-center">2020</td>
+            <td class="text-center">Yakes Jawa Timur</td>
+            <td class="text-center">OPEN</td>
+            <td class="text-center">1</td>
+          </tr>
+          <tr>
+            <td class="text-center">1</td>
+            <td class="text-center">Internal ISMS</td>
+            <td class="text-center">ISO200781:2002 TPK Sriwidjaya</td>
+            <td class="text-center">2020</td>
+            <td class="text-center">Yakes Jawa Timur</td>
+            <td class="text-center">OPEN</td>
+            <td class="text-center">1</td>
+          </tr>
+          <tr>
+            <td class="text-center">1</td>
+            <td class="text-center">Internal ISMS</td>
+            <td class="text-center">ISO200781:2002 TPK Sriwidjaya</td>
+            <td class="text-center">2020</td>
+            <td class="text-center">Yakes Jawa Timur</td>
+            <td class="text-center">OPEN</td>
+            <td class="text-center">1</td>
+          </tr>
+          <tr>
+            <td class="text-center">1</td>
+            <td class="text-center">Internal ISMS</td>
+            <td class="text-center">ISO200781:2002 TPK Sriwidjaya</td>
+            <td class="text-center">2020</td>
+            <td class="text-center">Yakes Jawa Timur</td>
+            <td class="text-center">OPEN</td>
+            <td class="text-center">1</td>
+          </tr>
+          <tr>
+            <td class="text-center">1</td>
+            <td class="text-center">Internal ISMS</td>
+            <td class="text-center">ISO200781:2002 TPK Sriwidjaya</td>
+            <td class="text-center">2020</td>
+            <td class="text-center">Yakes Jawa Timur</td>
+            <td class="text-center">OPEN</td>
+            <td class="text-center">1</td>
+          </tr>
+          <tr>
+            <td class="text-center">1</td>
+            <td class="text-center">Internal ISMS</td>
+            <td class="text-center">ISO200781:2002 TPK Sriwidjaya</td>
+            <td class="text-center">2020</td>
+            <td class="text-center">Yakes Jawa Timur</td>
+            <td class="text-center">OPEN</td>
+            <td class="text-center">1</td>
+          </tr>
         </tbody>
       </table>
     </div>
   </div>
+</div>
+
+<br>
+<div class="container">
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="#">4</a></li>
+      <li class="page-item"><a class="page-link" href="#">5</a></li>
+      <li class="page-item"><a class="page-link" href="#">6</a></li>
+      <li class="page-item"><a class="page-link" href="#">7</a></li>
+      <li class="page-item"><a class="page-link" href="#">8</a></li>
+      <li class="page-item"><a class="page-link" href="#">9</a></li>
+      <li class="page-item"><a class="page-link" href="#">10</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </div>
 
 
--- app/Views/pjw/tambah_data_audit.php
+++ app/Views/pjw/tambah_data_audit.php
@@ -4,6 +4,8 @@
 <!-- Untuk mulai mengisi konten yang akan ditampilkan -->
 <?= $this->section('content'); ?>
 
+<br>
+<br>
 <!-- Tambah Data Audit -->
 <div class="container">
   <div class="row">
@@ -13,24 +15,80 @@
   </div>
 </div>
 
+<br>
+
+
 <!-- Form Tambah Data -->
 <div class="container">
   <form>
+    <!-- Form Isi TPK -->
     <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-2 col-form-label"></label>
+      <label class="col-sm-2 col-form-label" for="exampleFormControlSelect1">TPK</label>
+      <div class="col-sm-8">
+        <select class="form-control " id="exampleFormControlSelect1">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <!-- Akhir Form Isi TPK -->
+    </div>
+    <!-- Form Isi PIC -->
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">PIC</label>
+      <div class="col-sm-10">
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <!-- Akhir Form Isi PIC -->
+    </div>
+    <!-- Form Isi Jenis -->
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Jenis</label>
+      <div class="col-sm-10">
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <!-- Akhir Form Isi Jenis -->
+    </div>
+    <!-- Form Isi Judul -->
+    <div class="form-group row">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">Judul</label>
       <div class="col-sm-10">
         <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
       </div>
+      <!-- Akhir Form Isi Judul -->
     </div>
+    <!-- Form Isi Status -->
     <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Status</label>
       <div class="col-sm-10">
-        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
       </div>
+      <!-- Akhir Form Isi Status -->
     </div>
     <div class="form-group row">
       <div class="col-sm-10">
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" class="btn btn-danger">Tambah Data</button>
+        <button type="submit" class="btn btn-primary">Kembali</button>
       </div>
     </div>
 </div>
